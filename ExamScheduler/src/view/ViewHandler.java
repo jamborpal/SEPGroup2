1862640c169bba7d9ce2f486befba1a9342b50b8
jamborpal <58312465+jamborpal@users.noreply.github.com>
2019-12-10 11:38:58 +0100

    things

--- ExamScheduler/src/view/ViewHandler.java
+++ ExamScheduler/src/view/ViewHandler.java
@@ -2,12 +2,5 @@ package view;
 
 public class ViewHandler
 {
-    private Scene currentScene;
-    private Stage primaryStage;
-    private ManageDataViewController manageDataViewController;
-    private DetailsViewController detailsViewController;
-    private CheckScheduleViewController checkScheduleViewController;
-    private PlanAnExamViewController planAnExamViewController;
-
 
 }
