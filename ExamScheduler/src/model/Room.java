9fbdbf1b5feaf3a564ebaf2e23b3aef5781aca1c
audrius145 <58250824+audrius145@users.noreply.github.com>
2019-12-10 09:14:30 +0100

    Hi

--- ExamScheduler/src/model/Room.java
+++ ExamScheduler/src/model/Room.java
@@ -8,12 +8,59 @@ public class Room
   private boolean hdmi;
   private boolean isArrangedW;
 
-  public Room(String roomName, int size, boolean vga, boolean hdmi, boolean isArrangedW)
+  public Room(String roomName, int size, boolean vga, boolean hdmi,
+      boolean isArrangedW)
   {
     this.roomName = roomName;
     this.size = size;
     this.vga = vga;
     this.hdmi = hdmi;
     this.isArrangedW = isArrangedW;
+  }
+
+  public boolean isVga()
+  {
+    return vga;
+  }
+
+  public boolean isHdmi()
+  {
+    return hdmi;
+  }
+
+  public boolean isArrangedForWritten()
+  {
+    return isArrangedW;
+  }
+
+  public String getRoomName()
+  {
+    return roomName;
+  }
+
+  public void setIsArrangedForWritten(boolean isArrangedW)
+  {
+    this.isArrangedW = isArrangedW;
+  }
+
+  public void setSize(int size)
+  {
+    this.size = size;
+  }
+
+  public boolean isCompatibleForOral()
+  {
+    return vga && hdmi;
+  }
+
+  public boolean equals(Object obj)
+  {
+    if(!(obj instanceof  Room))
+    {
+      return false;
+    }
+    Room other = (Room) obj;
+    return roomName.equals(other.roomName)
+        && size == other.size && vga == other.vga && hdmi == other.hdmi && isArrangedW == other.isArrangedW;
   }
 }
