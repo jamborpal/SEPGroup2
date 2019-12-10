9829eab7477786ae83a02d0560aa96636bb1cca4
audrius145 <58250824+audrius145@users.noreply.github.com>
2019-12-10 09:05:50 +0100

    Hi

--- ExamScheduler/src/model/Room.java
+++ ExamScheduler/src/model/Room.java
@@ -2,5 +2,18 @@ package model;
 
 public class Room
 {
+  private String roomName;
+  private int size;
+  private boolean vga;
+  private boolean hdmi;
+  private boolean isArrangedW;
 
+  public Room(String roomName, int size, boolean vga, boolean hdmi, boolean isArrangedW)
+  {
+    this.roomName = roomName;
+    this.size = size;
+    this.vga = vga;
+    this.hdmi = hdmi;
+    this.isArrangedW = isArrangedW;
+  }
 }
